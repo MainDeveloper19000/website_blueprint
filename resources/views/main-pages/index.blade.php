f122cd5a3eda503a78eaceb08970b6c77560a512
Your Name <MainDeveloper19000>
2024-10-28 13:48:01 +0700

    config

--- resources/views/main-pages/index.blade.php
+++ resources/views/main-pages/index.blade.php
@@ -91,7 +91,7 @@
         </div>
     </div>
     <!-- Modal Version 2 -->
-    <div class="modal modal-common-wrap fade" id="exampleModal2" tabindex="-1" aria-hidden="true">
+    <div class="modal modal-common-wrap fade" id="exampleModal2" tabindex="-1" aria-hidden="true">  
         <div class="modal-dialog modal-dialog-centered modal-xl">
             <div class="modal-content">
                 <div class="modal-header">
